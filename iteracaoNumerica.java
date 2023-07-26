c2ce171c035b45d3f57b2b104c3cbb121e5965eb
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-26 19:04:13 -0300

    Criando regra de negocio

--- iteracaoNumerica.java
+++ iteracaoNumerica.java
@@ -11,7 +11,11 @@ public class iteracaoNumerica {
 		System.out.println("Digite o número de casos que serão tratados: ");
 		int numCases = sc.nextInt();
 		
+		//Criando regra de negocio
 		
+		for (int i=1; i<=numCases; i++) {
+			System.out.printf("%d %d %d%n", i, i * i, i*i*i);
+		}
 		
 	}
 
