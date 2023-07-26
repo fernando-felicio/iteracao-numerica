d94f2576060237b102bf213f027cc2ec56f9ab8b
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-26 18:56:02 -0300

    Adicionando input

---
+++ iteracaoNumerica.java
@@ -0,0 +1,18 @@
+package iteracao_numerica;
+import java.util.Scanner;
+
+public class iteracaoNumerica {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Recebendo input do usuario
+		
+		System.out.println("Digite o número de casos que serão tratados: ");
+		int numCases = sc.nextInt();
+		
+		
+		
+	}
+
+}
